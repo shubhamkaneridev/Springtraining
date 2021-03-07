f70747c3e17984fbc7501b2defbfcef215ce0d59
shubhamkaneridev <shubhamkaneri24@gmail.com>
2021-03-07 08:06:56 +0530

    This is Jdbc Spring Stored procedure

---
+++ SpringJdbcStoredProessure/src/com/training/jdbcTemplate/StudentJDBCTemplate.java
@@ -0,0 +1,51 @@
+package com.training.jdbcTemplate;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+
+import com.training.dao.StudentDAO;
+import com.training.mapper.StudentMapper;
+import com.training.pojo.Student;
+
+
+public class StudentJDBCTemplate implements StudentDAO {
+	private DataSource dataSource;
+	private SimpleJdbcCall jdbcCall;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcCall =  new SimpleJdbcCall(dataSource).withProcedureName("getRecord");
+	}
+	public void create(String name, Integer age) {
+		JdbcTemplate jdbcTemplateObject = new JdbcTemplate(dataSource);
+		String SQL = "insert into Student (name, age) values (?, ?)";
+
+		jdbcTemplateObject.update( SQL, name, age);
+		System.out.println("Created Record Name = " + name + " Age = " + age);
+		return;
+	}
+	public Student getStudent(Integer id) {
+		SqlParameterSource in = new MapSqlParameterSource().addValue("in_id", id);
+		Map<String, Object> out = jdbcCall.execute(in);
+
+		Student student = new Student();
+		student.setId(id);
+		student.setName((String) out.get("out_name"));
+		student.setAge((Integer) out.get("out_age"));
+		return student;
+	}
+	public List<Student> listStudents() {
+		JdbcTemplate jdbcTemplateObject = new JdbcTemplate(dataSource);
+		String SQL = "select * from Student";
+		List <Student> students = jdbcTemplateObject.query(SQL, new StudentMapper());
+		return students;
+	}
+}
